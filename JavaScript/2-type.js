30bee773c7da67be55618da824d0b9d99a5a6f52
tshemsedinov <timur.shemsedinov@gmail.com>
2018-02-28 22:54:49 +0200

    Add debud output

--- JavaScript/2-type.js
+++ JavaScript/2-type.js
@@ -9,6 +9,7 @@ const obj1 = {
   name: 'Aurelius',
   [Symbol('name')]: 'Marcus',
   [Symbol('name')]: 'Aurelius',
+  [Symbol('name')]: Symbol('value'),
 };
 const key = Symbol('name');
 obj1[key] = 'Antoninus';
@@ -16,7 +17,8 @@ console.log('obj1[key] = ' + obj1[key]);
 console.dir(obj1);
 console.log('typeof = ' + typeof(obj1));
 
-console.log('keys = ' + Object.keys(obj1));
+console.log({ keys: Object.keys(obj1) });
 for (const key in obj1) {
   console.log('Key in obj1: ' + key);
+  console.log('value: ' + obj1[key]);
 }
